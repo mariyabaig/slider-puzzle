c4c05486b654d67168e3795236e926cea290a9f0
mariyabaig <mariyabaig0106@gmail.com>
2024-02-07 17:43:19 +0530

    styles updated

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,38 +1,52 @@
-
-import "../index.css"
+import "../index.css";
 
 const Rules = () => {
   return (
     <div className="container">
       <h2 className="heading">Rules of the Game</h2>
       <p className="intro">
-        Welcome to the Game! The objective is to rearrange the puzzle
-        pieces to reach the correct order.
+       The objective is to rearrange the puzzle pieces to
+        reach the correct order.
       </p>
       <div className="section">
         <strong className="subHeading">How to Play:</strong>
-        <ol className="list">
-          <li>Click on a puzzle piece next to the empty slot to move it.</li>
-          <li>
-            Continue making moves until you successfully arrange the numbers in
-            ascending order.
-          </li>
-          <li>
-            To start a new game, click the "Reshuffle" button. The puzzle will
-            be shuffled, and you can begin again.
-          </li>
-        </ol>
+        <div className="list">
+          <p>
+            &#11088; Click on a puzzle piece next to the empty slot to move it.
+          </p>
+        </div>
+        <div className="list">
+          <p>
+            &#11088; Continue making moves until you successfully arrange the
+            numbers in ascending order.
+          </p>
+        </div>
+        <div className="list">
+          <p>
+            {" "}
+            &#11088; To start a new game, click the "Reshuffle" button. The
+            puzzle will be shuffled, and you can begin again.
+          </p>
+        </div>
       </div>
       <div className="section">
         <strong className="subHeading">Rules:</strong>
-        <ul className="list">
-          <li>Only adjacent puzzle pieces can be moved into the empty slot.</li>
-          <li>Strive to complete the puzzle in the fewest moves possible.</li>
-        </ul>
+        <div className="list">
+          <p>
+            {" "}
+            &#11088;Only adjacent puzzle pieces can be moved into the empty
+            slot.
+          </p>
+        </div>
+        <div className="list">
+          <p>
+            &#11088; Strive to complete the puzzle in the fewest moves possible.
+          </p>
+        </div>
       </div>
-      <p className="footer">Have fun and enjoy the challenge!</p>
+     Have fun and enjoy the challenge!
     </div>
-  )
-}
+  );
+};
 
 export default Rules;
